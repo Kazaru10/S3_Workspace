ceecf8209bdf61b0f86be25c51a9f31d138a154b
J.ZEPHIR <106028097+Kazaru10@users.noreply.github.com>
2023-04-10 16:43:29 +0200

    Create S2_RBT_ddl.py
    Script to automatically download S3A_RBT products from Copernicus datas

---
+++ S2_RBT_ddl.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import date
+
+
+#################
+
+username = "NAME"
+password = "PASS"
+
+#################
+
+Day = date.today()
+
+# SENTIENL-3(A) RBT
+Req_S3 = "https://scihub.copernicus.eu/dhus/odata/v1/Products?$filter=IngestionDate gt datetime'" + str(Day) + "T00:00:00.000' and substringof('S3A_SL_1_RBT',Name) and substringof('_PS1_O_NT',Name)"
+
+result = requests.get(Req_S3, auth = HTTPBasicAuth(username, password))
