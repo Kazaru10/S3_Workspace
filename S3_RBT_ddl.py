ae904a004ee1c6645d3f67039a2aa7b110ba03d3
J.ZEPHIR <106028097+Kazaru10@users.noreply.github.com>
2023-04-10 17:31:16 +0200

    Update S3_RBT_ddl.py
    Reading xml results

--- S3_RBT_ddl.py
+++ S3_RBT_ddl.py
@@ -3,6 +3,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from datetime import date
+import xml.etree.ElementTree as ET
 
 
 #################
@@ -18,3 +19,7 @@ Day = date.today()
 Req_S3 = "https://scihub.copernicus.eu/dhus/odata/v1/Products?$filter=IngestionDate gt datetime'" + str(Day) + "T00:00:00.000' and substringof('S3A_SL_1_RBT',Name) and substringof('_PS1_O_NT',Name)"
 
 result = requests.get(Req_S3, auth = HTTPBasicAuth(username, password))
+
+tree = ET.fromstring(result.text)
+
+
