456b1982276f53512082d3290c46cb6f012d4bcd
J.ZEPHIR <106028097+Kazaru10@users.noreply.github.com>
2023-05-01 18:43:50 +0200

    Update DailyShow.py

--- DailyShow.py
+++ DailyShow.py
@@ -2,26 +2,54 @@
 
 from flask import Flask, render_template
 import os
+import random
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 app = Flask(__name__)
 
 img = os.path.join('static', 'Image')
 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(print_date_time, IntervalTrigger(seconds=10))
+scheduler.start()
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Coucou, Monde!</p>"
 
-# S3
+## S3
 @app.route("/S3_RBT")
 def display_S3():
-    file = os.path.join(img, 'img.jpg')
-    return render_template('template.html', image=file)
-
-# S2
+    path = os.path.join('static', 'Image', 'S3')
+    dir_list = os.listdir(path)
+    n = random.randint(0, len(dir_list) - 1)
+    
+    file = os.path.join(img, dir_list[n])
+    return render_template('template_S3.html', image=file)
+    
+## S2
 @app.route("/S2_TCI")
+def display_S2():
+    path = os.path.join('static', 'Image', 'S2')
+    dir_list = os.listdir(path)
+    n = random.randint(0, len(dir_list) - 1)
+    
+    file = os.path.join(img, dir_list[n])
+    return render_template('template_S2.html', image=file)
 
-app.run()
+
+   
+if __name__ == '__main__':
+    app.run()
+
+## Shut down the scheduler
+scheduler.shutdown(wait=False)
 
 
 # See link below
